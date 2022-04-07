c689ea940a181ad1ef711e52df3bf4a8863ee599
H-code9 <akanohabib066@gmail.com>
2022-04-07 23:30:27 +0100

    Task 5 FIX

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
- /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
- int main(void)
+/**
+ * main - Entry point
+ * My hello, printf task
+ * Return: return 1 after program execution
+ */
+int main(void)
 {
-puts("\"with proper grammar, but the outcome is a piece of art,/n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
-}
+} 
