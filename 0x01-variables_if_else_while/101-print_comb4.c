8c9d4efc99692a464b15a464e2f3906ab1a80d56
H-code9 <akanohabib066@gmail.com>
2022-04-11 15:04:17 +0100

    ADVANCE TASK is a banger

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -4,9 +4,9 @@
  * Description: prints all possible different combinations of three digits.
  * Numbers must be separated by ,, followed by a space.
  * The three digits must be different.
- * 012, 120, 102, 021, 201, 210 are considered the same combination of the three digits 0, 1 and 2.
+ * 012, 120, 102, 021, 201, 210 are considered the same combination.
  * Print only the smallest combination of three digits.
- * Numbers should be printed in ascending order, with three digits.
+ * Numbers should be printed in ascending order.
  * You can only use the 'putchar' to print console.
  * You can only use the 'putchar' up to 6 times.
  * You are not allowed to use any variable of type 'char'.
