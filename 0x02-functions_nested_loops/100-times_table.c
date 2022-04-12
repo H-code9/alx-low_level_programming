9c9debe5292af68a3698355d1da4f0ef887f8a5b
H-code9 <akanohabib066@gmail.com>
2022-04-12 23:43:09 +0100

    100-times_table.c 22

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,28 +17,28 @@ putchar('0');
 
 for (mult = 1; mult <= n; mult++)
 {
-putchar(',');
-putchar(' ');
+_putchar(',');
+_putchar(' ');
 
 prod = num * mult;
 
 if (prod <= 99)
-putchar(' ');
+_putchar(' ');
 if (prod <= 9)
-putchar(' ');
+_putchar(' ');
 
 if (prod >= 100)
 {
-putchar((prod / 100) + '0');
-putchar(((prod / 10)) % 10 + '0');
+_putchar((prod / 100) + '0');
+_putchar(((prod / 10)) % 10 + '0');
 }
 else if (prod <= 99 && prod >= 10)
 {
-putchar((prod / 10) + '0');
+_putchar((prod / 10) + '0');
 }
-putchar((prod % 10) + '0');
+_putchar((prod % 10) + '0');
 }
-putchar('\n');
+-putchar('\n');
 }
 }
 }
