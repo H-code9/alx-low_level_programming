57f9fef7d898b4580c904c1d6952e348eeb077c8
H-code9 <akanohabib066@gmail.com>
2022-04-13 18:07:37 +0100

    debug 4th trial

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@ int main(void)
 {
 int i;
 
-printf("infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
 i = 0;
 
@@ -16,7 +16,7 @@ i = 0;
 *while (i < 10)
 *{
 *Infinite loop - No increase of variable
-*putchar(1);
+*putchar(i);
 }
 */
 printf("Infinite loop avoided! \\o/\n");
