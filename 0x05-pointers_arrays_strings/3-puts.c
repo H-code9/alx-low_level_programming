11bba31e0eb773971f0de424dd99984fe1066c5f
H-code9 <akanohabib066@gmail.com>
2022-04-21 17:39:00 +0100

    task 4

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,9 @@
+#include <unistd.h>
+/**
+ *_puts - prints a string
+ *@str: A pointer to an int that will be changed
+ *
+ *Return: void which means our answer is correct
+ */
+
+void _puts(char *str);
