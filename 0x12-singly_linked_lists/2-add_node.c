28e492db4c17483164d1a48e5881cb2076bca872
H-code9 <akanohabib066@gmail.com>
2022-05-26 15:30:56 +0100

    fixed task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,7 +26,8 @@ list_t *add_node(list_t **head, const char *str)
 	*head = new;
 
 	return (*head);
-}#include "lists.h"
+}
+#include "lists.h"
 /**
  * add_node - adds a new node at the beginning
  * of a list_t list.
